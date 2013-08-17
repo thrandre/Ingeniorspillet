7dc8fb49d8fe9d0d9df02da4300acd354c069413
thrandre <thomas.r.andresen@gmail.com>
2013-08-17 20:51:32 +0200

    Cleanup

--- app/core/core.ts
+++
@@ -1,5 +0,0 @@
-﻿///<reference path="utils.ts"/>
-///<reference path="observable.ts"/>
-///<reference path="property.ts"/>
-///<reference path="promise.ts"/>
-///<reference path="objects.ts"/>
--- app/hacks.js
+++
@@ -1,5 +0,0 @@
-﻿Object.prototype.getType = function () {
-    var results = /function (.{1,})\(/.exec((this).constructor.toString());
-    return (results && results.length > 1) ? results[1] : "";
-};
-//@ sourceMappingURL=hacks.js.map
